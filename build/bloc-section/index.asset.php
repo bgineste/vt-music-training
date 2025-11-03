25f3eddc303215a83f4031b085806b91e020358e
bgineste <bernard.gineste@gmail.com>
2025-11-03 19:21:51 +0100

    retouches couleurs de l'éditeur et du site (fond de section et d'oeuvre)

--- build/bloc-section/index.asset.php
+++ build/bloc-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9b1d4e88ffc87c2cb353');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'dd0f61fdbc91d73e7c07');
